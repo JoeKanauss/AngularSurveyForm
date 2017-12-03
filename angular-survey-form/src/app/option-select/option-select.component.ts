4dcb6add3b802ef76ac70490f904ebc573f1083c
JoeKanauss <33582866+JoeKanauss@users.noreply.github.com>
2017-12-02 19:45:49 -0600

    New changes
    Updated pages for dropdown survey form

--- angular-survey-form/src/app/option-select/option-select.component.ts
+++ angular-survey-form/src/app/option-select/option-select.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { OPTION_SELECT } from '../optionSelect';
-import { option } from '../option';
+
 @Component({
   selector: 'app-option-select',
   templateUrl: './option-select.component.html',
@@ -8,19 +7,21 @@ import { option } from '../option';
 })
 export class OptionSelectComponent implements OnInit {
 	
-  Option: option = [ {value: 1, name:'1. Best time'},
-					 {value: 2, name:'2. Next Best time'},
-					 {value: 3, name:'3. Not as good'},
-					 {value: 4, name:'4. Worst time'} ];
-  
-	removeOption(){
-		var optionToRemove = document.getElementById("dropdown").value;
-		
-		alert(optionToRemove);
+	 optionArray: string[]= [ "1. Best time",
+				"2. Next Best time",
+				"3. Not as good",
+				"4. Worst time"];
+
+ public removeOption($event, optionArray: string[]){
+		var toRemove = event.target.value;
+		alert("Index to Remove: " + toRemove);
+		optionArray.splice(optionArray[toRemove],1);
 	}
 	
-  constructor() { }
+  constructor() {
 
+		  }
+  
   ngOnInit() {
   }
 
